3dee15a3081872d2a6b2e6b6c374a8ab414954c0
optiplex9010 <liangvw@gmail.com>
2018-12-19 13:16:05 +1100

    first commit
    - still need to find ways to label the convergence lines nicely

---
+++ Brownian motion/convergence_plot.jl
@@ -0,0 +1,44 @@
+using PyPlot
+# using PyCall
+# @pyimport matplotlib.patches as patch
+
+@doc """
+	guideplot(N, s)
+
+Returns a plot with 4 convergence lines
+N: Maximum number of boundary partitions
+s: constant scaling
+""" -> 
+function guideplot(N, s = 0.05)
+	plt[:xscale]("log")
+	plt[:yscale]("log")
+	grid("on", lw = 0.5)
+	plot(1:N, 1 ./((1:N).^0.5)*s, label = L"$1/\sqrt{n}$", ls = "--", color = "black")
+	plot(1:N, 1 ./(1:N)*s, label = L"1/n", ls = "--", color = "black")
+	plot(1:N, 1 ./((1:N).^2)*s, label = L"1/n^2", ls = "--", color = "black")
+	plot(1:N, 1 ./((1:N).^4)*s, label = L"1/n^4", ls=  "--", color = "black")
+	xticks(unique(vcat(1:10, 10:10:100)))
+end
+
+
+@doc """
+	converge(n, N)
+
+Returns a convergence plot of the MC approximation towards the true solution
+n: number of equally spaced points
+N: maximum number of boundary partitions
+""" -> 
+function converge(n, N)
+limit = exact_limit()
+n_mesh = unique(floor.(Int,exp.( log(N)*( 0:(1/(n-1)):1 ) ) ) ) # cuts up the x - log axis uniformly to save computation
+bcp_vec = zeros(length(n_mesh))
+for i in 1:length(n_mesh)
+	bcp_vec[i] = abs(BCP(n_mesh[i],1/n_mesh[i]) - limit)
+end
+figure()
+guideplot(N, bcp_vec[1])
+plot(n_mesh, bcp_vec, marker="o")
+xlabel("n")
+ylabel(L"|P_n - P|")
+title(L"Error, $|P - P_n|$")
+end
