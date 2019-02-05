40a1463f1524ce3031706227a7dede2ba04c4dcf
optiplex9010 <liangvw@gmail.com>
2019-02-05 22:31:50 +1100

    added initial point
    added initial point parameter to test perturbation of bcp

--- Brownian motion/integral_equation_loader_deely.jl
+++ Brownian motion/integral_equation_loader_deely.jl
@@ -27,21 +27,31 @@ function exact_limit(T = 1)
 end
 
 @doc """
-	g(t, x0, n)
+	g(t, x0, t0, n)
 
 n-piecewise linear approximation of g(t)
 n: Maximum number of boundary partitions
 """ -> 
-function g(t, x0, n = 0)
+function g(t, x0, t0, n = 0)
 if n == 0
-	return g(t) - x0
+	return g(t + t0) - x0
+else
+	ti = floor.(t.*n)./n
+	m = n.*(g(ti + 1 ./n) - g(ti))
+	c = g(ti) - ti.*m
+	return m.*(t-t0) + c
 end
-ti = floor(t.*n)/n
-m = n.*(g(ti + 1 ./n) - g(ti))
-c = g(ti) - ti.*m
-	return m.*t + c
 end
 
+t0 = 0
+N = 1000
+t_vec = range(t0; stop = 1, length = N)
+vector = zeros(N)
+for i in 1:N
+	vector[i] = g(t_vec[i], 0, t0, 3) 
+end
+
+plot(t_vec, vector)
 
 function K_gen(N, n = 0, T = 1, x0 = 0, t0 = 0)
 h = (T-t0)/N
@@ -52,14 +62,16 @@ function b(t)
 end
 for j = 1:N
 	for i = 1:j
-		t = t0 + j*h
-		s = t0 + (2i-1)*h/2
-		K[j,i] =  cdf(Normal(0,1), ( g(s,x0,n) - g(t,x0,n) )/sqrt(t-s)) + exp( -2*b(t)*( g(t,x0,n) - g(s,x0,n) - (t-s)*b(t) ) )*
-			cdf(Normal(0,1), ( g(s,x0,n) - g(t,x0,n) + 2*(t-s)*b(t) )/sqrt(t-s))
+		t = j*h
+		s = (2i-1)*h/2
+		K[j,i] = cdf(Normal(0,1), ( g(s,x0,t0,n) - g(t,x0,t0,n) )/sqrt(t-s)) + 
+				exp( -2*b(t)*( g(t,x0,t0,n) - g(s,x0,t0,n) - (t-s)*b(t) ) )*
+				cdf(Normal(0,1), ( g(s,x0,t0,n) - g(t,x0,t0,n) + 2*(t-s)*b(t) )/sqrt(t-s))
 	end
 end
 return K
 end
+
 
 function F_vec(N, n = 0, T = 1, x0 = 0, t0 = 0)
 h = (T-t0)/N
@@ -69,8 +81,9 @@ function b(t)
 	return 0
 end
 for i = 1:N
-	t = t0 + i*h
-	F[i] = cdf(Normal(0,1), -g(t,x0,n)/sqrt(t)) + exp(-2*b(t)*(g(t,x0,n) - t*b(t)))*cdf(Normal(0,1), (-g(t,x0,n) + 2t*b(t))/sqrt(t))
+	t = i*h
+	F[i] = cdf(Normal(0,1), -g(t,x0,t0,n)/sqrt(t)) + 
+			exp(-2*b(t)*(g(t,x0,t0,n) - t*b(t)))*cdf(Normal(0,1), (-g(t,x0,t0,n) + 2t*b(t))/sqrt(t))
 end
 return F
 end
@@ -82,7 +95,9 @@ Returns approximated boundary crossing probability (Loader & Deely 1987)
 N: Number of partitions
 n: Number of piecewise linear partitions. Set to 0 to keep the original boundary.
 T: Terminal time
+x0: initial position of BM
+t0: inital time of BM
 """ -> 
 function BCP_LD(N, n = 0, T = 1, x0 = 0, t0 = 0)
-	return sum(K_gen(N, n, T)\F_vec(N, n, T, x0, t0))
+	return sum(K_gen(N, n, T, x0, t0)\F_vec(N, n, T, x0, t0))
 end
